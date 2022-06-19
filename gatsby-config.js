c34d38c44101b3d640db8656ef46076f99cda24d
Nilo Cara <nilo.cara@gmail.com>
2022-06-19 19:15:15 +0200

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  siteMetadata: {
+    description: "Personal page of Nilo Cara",
+    locale: "en",
+    showThemeLogo: false,
+    title: "Nilo Cara",
+    formspreeEndpoint: "https://formspree.io/f/xbjwbbqv",
+  },
+    plugins: [
+      `gatsby-plugin-image`,
+      `gatsby-plugin-sharp`,
+      `gatsby-transformer-sharp`,
+      {
+        
+        resolve: "@wkocjan/gatsby-theme-intro",
+        options: {
+          theme: "dark-pink",
+        },
+      },
+    ],
+  }
